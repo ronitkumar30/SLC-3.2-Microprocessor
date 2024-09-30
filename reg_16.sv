97cc594ecef76ad33996081abdcbafea04776b96
ronitkumar30 <107587291+ronitkumar30@users.noreply.github.com>
2024-09-30 11:59:33 -0700

    Update reg_16.sv

--- reg_16.sv
+++ reg_16.sv
@@ -1,4 +1,4 @@
-'''Purpose: A 16-bit register used for PC, MDR, and general-purpose registers 
+```Purpose: A 16-bit register used for PC, MDR, and general-purpose registers 
   
   module reg_16(input Clk, Reset, Load, [15:0] D, output reg [15:0] Data_Out); 
     always @(posedge Clk or posedge Reset) begin 
