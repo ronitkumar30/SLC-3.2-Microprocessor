1d6d25195e987a3b8f63a04d936189d7caddb43d
ronitkumar30 <107587291+ronitkumar30@users.noreply.github.com>
2024-09-30 11:56:32 -0700

    Create reg_16.sv

---
+++ reg_16.sv
@@ -0,0 +1,8 @@
+'''Purpose: A 16-bit register used for PC, MDR, and general-purpose registers 
+  
+  module reg_16(input Clk, Reset, Load, [15:0] D, output reg [15:0] Data_Out); 
+    always @(posedge Clk or posedge Reset) begin 
+      if (Reset) Data_Out <= 16'd0; 
+      else if (Load) Data_Out <= D; 
+    end 
+  endmodule
